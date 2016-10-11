875a7928161e1594ff76221fc51bcdab9b494646
L0cust <l0custdev@gmail.com>
2016-10-11 21:40:37 +0100

    Remove comments

--- src/Utilities/Maths.java
+++ src/Utilities/Maths.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Utilities;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-/**
- *
- * @author Bruno
- */
 public class Maths {
 
     public static BigDecimal sqrt(BigDecimal val, int precision) {
