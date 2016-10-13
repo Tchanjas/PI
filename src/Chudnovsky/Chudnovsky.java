526abb72d41d49ba880e6af6a9b1453b24e78c51
L0cust <l0custdev@gmail.com>
2016-10-13 11:30:51 +0100

    Fix sequential Chudnovsky calculation

--- src/Chudnovsky/Chudnovsky.java
+++ src/Chudnovsky/Chudnovsky.java
@@ -6,49 +6,50 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 
 public class Chudnovsky {
-    
-    MathContext context;
-    static int sqrtPrec; // precision for the Maths.sqrt method
-    
-    // Constants of the algorithm
-    BigDecimal CONST1 = BigDecimal.valueOf(13591409);
-    BigDecimal CONST2 = BigDecimal.valueOf(545140134);
-    BigDecimal CONST3 = BigDecimal.valueOf(640320);
-    BigDecimal CONST4 = Maths.sqrt(BigDecimal.valueOf(640320).pow(3), sqrtPrec);
-
-    BigDecimal SIX = BigDecimal.valueOf(6);
-    BigDecimal THREE = BigDecimal.valueOf(3);
-    BigDecimal MONE = BigDecimal.valueOf(-1);
-    BigDecimal TWELVE = BigDecimal.valueOf(12);
-
-    // Elements of each part of the algorithm
-    BigDecimal first;
-    BigDecimal second;
-    BigDecimal third;
-    BigDecimal fourth;
-    BigDecimal fifth;
-    BigDecimal sixth;
-
-    BigDecimal num;
-    BigDecimal denum;
-
-    BigDecimal result = BigDecimal.ZERO;
 
     /**
      * Calculate PI with the Chudnovsky algorithm in a sequencial execution
+     *
      * @param iter the number of k of the sum
-     * @param precision the number of digits to be used for an operation; results are rounded to this precision
+     * @param precision the number of digits to be used for an operation;
+     * results are rounded to this precision
      * @return the value of PI in BigDecimal
      */
     public BigDecimal calcPI(int iter, int precision) {
+
+        MathContext context;
+
+        // Constants of the algorithm
+        BigDecimal CONST1 = BigDecimal.valueOf(13591409);
+        BigDecimal CONST2 = BigDecimal.valueOf(545140134);
+        BigDecimal CONST3 = BigDecimal.valueOf(640320);
+        BigDecimal CONST4 = Maths.sqrt(BigDecimal.valueOf(640320).pow(3), precision);
+
+        BigDecimal SIX = BigDecimal.valueOf(6);
+        BigDecimal THREE = BigDecimal.valueOf(3);
+        BigDecimal MONE = BigDecimal.valueOf(-1);
+        BigDecimal TWELVE = BigDecimal.valueOf(12);
+
+        // Elements of each part of the algorithm
+        BigDecimal first;
+        BigDecimal second;
+        BigDecimal third;
+        BigDecimal fourth;
+        BigDecimal fifth;
+        BigDecimal sixth;
+
+        BigDecimal num;
+        BigDecimal denum;
+
+        BigDecimal result = BigDecimal.ZERO;
+
         context = new MathContext(precision);
-        sqrtPrec = precision;
-        
+
         // Factorials for each factorial function in the algorithm
         SharedFactorial sharedFact_1 = new SharedFactorial();
         SharedFactorial sharedFact_2 = new SharedFactorial();
         SharedFactorial sharedFact_3 = new SharedFactorial();
-        
+
         // the sum of the formula
         for (int i = 0; i < iter; i++) {
             first = MONE.pow(i, context);
@@ -64,7 +65,7 @@ public class Chudnovsky {
 
             result = result.add(num.divide(denum, context));
         }
-        
+
         // result = ((sum of the algorithm)^-1)/12
         result = result.pow(-1, context).divide(TWELVE, context);
         return result;
